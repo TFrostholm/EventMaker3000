778885484db6d12753f9b211db5accba8cb8b4b8
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-02-09 16:24:47 +0100

    Added code to Event.cs

--- EventMaker3000/EventMaker3000/Model/Event.cs
+++ EventMaker3000/EventMaker3000/Model/Event.cs
@@ -1,7 +1,26 @@
-﻿namespace EventMaker3000.Model
+﻿using System;
+
+namespace EventMaker3000.Model
 {
     public class Event
     {
-         
+        // Properties:
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Place { get; set; }
+        public DateTime DateTime { get; set; }
+
+
+        //Constructor:
+        public Event(int id, string name, string description, string place, DateTime dateTime)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Place = place;
+            DateTime = dateTime;
+        }
     }
 }
