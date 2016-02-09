35f7e6875ed4667119a39f297fa258c50a00da02
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-02-09 16:36:28 +0100

    Added code to DateTimeConverter

--- EventMaker3000/EventMaker3000/Converter/DateTimeConverter.cs
+++ EventMaker3000/EventMaker3000/Converter/DateTimeConverter.cs
@@ -1,7 +1,15 @@
-﻿namespace EventMaker3000.Converter
+﻿using System;
+
+namespace EventMaker3000.Converter
 {
     public class DateTimeConverter
     {
-         
+        // the job here is to get some input from datepicker and timepicker. so we can click in the gui so the input there should go here so it can be converted
+        // It makes a DateTime object
+        public static DateTime DateTimeOffsetAndTimeSetToDateTime(DateTimeOffset date, TimeSpan time)
+        {
+            return new DateTime(date.Year, date.Month, date.Day, time.Hours, time.Minutes, 0);
+        }
+
     }
 }
