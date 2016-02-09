eae7a4ec9b4a7d807ded6b1b3bb9a92dcef992cf
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-02-09 17:26:44 +0100

    Fixed xaml pages and data context

--- EventMaker3000/EventMaker3000/App.xaml.cs
+++ EventMaker3000/EventMaker3000/App.xaml.cs
@@ -14,7 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using EventMaker.View;
+using EventMaker3000.View;
 
 namespace EventMaker3000
 {
